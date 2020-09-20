426daa38d9875982cce5a4cc8edca84883725c4f
Symeon Quimby <kershoc@gmail.com>
2020-09-20 00:03:36 -0400

    resistor-color-duo exercise

---
+++ resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,17 @@
+BAND_VALUES = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+
+
+def value(colors):
+    (first, second, *_) = colors
+    return BAND_VALUES[first]*10 + BAND_VALUES[second]
