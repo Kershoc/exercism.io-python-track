48615848f08734ebee12f5339ccab2a8dc958db9
Symeon Quimby <kershoc@gmail.com>
2020-09-19 23:47:01 -0400

    resistor-color exercise

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+BAND_VALUES = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+
+
+def color_code(color):
+    return BAND_VALUES[color]
+
+
+def colors():
+    return list(BAND_VALUES.keys())
