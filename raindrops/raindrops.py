34d9143b80eb1d98077916155c16055514bd2ed9
Symeon Quimby <kershoc@gmail.com>
2020-09-19 22:01:46 -0400

    raindrops exercise

---
+++ raindrops/raindrops.py
@@ -0,0 +1,15 @@
+def convert(number):
+    output = ''
+    if number % 3 == 0:
+        output += "Pling"
+
+    if number % 5 == 0:
+        output += "Plang"
+
+    if number % 7 == 0:
+        output += "Plong"
+
+    if output == '':
+        output = f"{number}"
+
+    return output
