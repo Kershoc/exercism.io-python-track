f371225add15264a684303084975af39aa8b7690
Symeon Quimby <kershoc@gmail.com>
2020-09-21 00:35:52 -0400

    perfect_numbers exercise

---
+++ perfect-numbers/perfect_numbers.py
@@ -0,0 +1,17 @@
+def classify(number):
+    if number < 1:
+        raise ValueError("Positive Numbers Only")
+
+    aliquotSum = 1
+    i = 2
+    
+    while i**2 < number:
+        if not number % i:
+            aliquotSum += i + number/i
+        i += 1
+
+    if aliquotSum == number and aliquotSum != 1:
+        return "perfect"
+    if aliquotSum > number:
+        return "abundant"
+    return "deficient"
